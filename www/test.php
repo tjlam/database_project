0967d5d8c7cc8bd8f5f3d76c6d9104ad69695b22
Tyler Lam <tylerlam888@gmail.com>
2017-07-17 16:42:37 -0400

    config change

--- www/test.php
+++ www/test.php
@@ -19,47 +19,47 @@
     </style>
   </head>
   <body>
-    <div id="map"></div>
-    <script>
+    <div id="map" style="width: 500px; height: 400px;"></div>
 
-      var locations = [
-        ['1802kng', '-80.5260442', '43.4791858'],
-        ['305lester', '-80.5325125', '43.4713316']
-      ];
-      function initMap() {
-        // Try HTML5 geolocation.
+    <script type="text/javascript">
+
+      //use html5 to get location
+      var lat,lng;
+      getLocation();
+      function getLocation() {
         if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position, plotLocation) {
-            var currPos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
+            navigator.geolocation.getCurrentPosition(showPosition);
+            // initMap();
         } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
+            alert("Geolocation is not supported by this browser.");
         }
       }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
+      function showPosition(position) {
+        lat = position.coords.latitude;
+        lng = position.coords.longitude;
+        console.log(lat);
+        initMap();
       }
-      var plotLocation = function() {
-        var map, infoWindow;
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: new google.maps.LatLng(currPos.lat, currPos.lng),
-          zoom: 15,
+
+      //create map
+      function initMap() {
+        var locations = [
+          ['Bondi Beach', -33.890542, 151.274856],
+          ['Coogee Beach', -33.923036, 151.259052],
+          ['Cronulla Beach', -34.028249, 151.157507],
+          ['Manly Beach', -33.80010128657071, 151.28747820854187],
+          ['Maroubra Beach', -33.950198, 151.259302]
+        ];
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: new google.maps.LatLng(-33.92, 151.25),
           mapTypeId: google.maps.MapTypeId.ROADMAP
         });
-        infoWindow = new google.maps.InfoWindow;
+
+        var infowindow = new google.maps.InfoWindow();
 
         var marker, i;
+
         for (i = 0; i < locations.length; i++) {
           marker = new google.maps.Marker({
             position: new google.maps.LatLng(locations[i][1], locations[i][2]),
@@ -75,8 +75,9 @@
         }
       }
     </script>
+
     <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJCIwyI8OK5-gWujsqqNHk6fVktg5PBzg&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJCIwyI8OK5-gWujsqqNHk6fVktg5PBzg">
     </script>
   </body>
 </html>
