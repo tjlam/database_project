5ebe691fa9a5872ea35994ef89dd8fd0d5512652
Tyler Lam <tylerlam888@gmail.com>
2017-07-18 14:55:00 -0400

    added map functionality to nearby page

--- www/nearby.php
+++ www/nearby.php
@@ -12,6 +12,22 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- YO KEVIN! this is styling for the map. make it look nice lol  -->
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 50%;
+        width: 50%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+
+    </style>
   </head>
   <body>
     <div class = "col">
@@ -55,7 +71,7 @@
       $washrooms = array();
       // retrieve washrooms
       $query = "
-        SELECT wash.latitude, wash.longitude, wash.building, wash.room_num,
+        SELECT wash.id, wash.latitude, wash.longitude, wash.building, wash.room_num,
         wash.description, wash.gender, ROUND(AVG(rev.rating)) as avg_rtg
         FROM washrooms as wash
         LEFT JOIN reviews as rev
@@ -67,10 +83,12 @@
       $stmt = $mysqli->prepare($query);
       $stmt->bind_param('s', $gdr);
       $stmt->execute();
-      $stmt->bind_result($lat, $lng, $bld, $room, $desc, $gdr, $rtg);
+      $stmt->bind_result($id, $lat, $lng, $bld, $room, $desc, $gdr, $rtg);
+      //array stores the location data to be used by google maps
+      $locations = array();
       //loop through results and create washroom object
       while ($stmt->fetch()) {
-        $washroom = new Washroom($lat, $lng, $bld, $room, $desc, $gdr, $rtg );
+        $washroom = new Washroom($id, $lat, $lng, $bld, $room, $desc, $gdr, $rtg );
         // var_dump($washroom);
         //calculate and set distance from current loc. to washroom
         $washroom->distance = getDistance(
@@ -81,7 +99,12 @@
         );
         // echo $washroom->distance;
         $washrooms[] = $washroom;
+        // add to locations array
+        array_push($locations, (array($id, $lat, $lng)));
       }
+      //convert locations array to json
+      $myJSON = json_encode($locations);
+      // var_dump($myJSON);
       // function compares two washroom objects by distance
       function cmpDistance ($a, $b) {
         return ($a->distance) - ($b->distance);
@@ -90,7 +113,6 @@
       usort($washrooms, "cmpDistance");
       // var_dump($washrooms);
       // print out all washrooms
-
       foreach ($washrooms as $w) {
         echo  $w->print_washroom();
         ?>
@@ -110,5 +132,61 @@
   </div>
   </div>
   </div>
+  <div id="map"></div>
+
+  <script>
+    var lat,lng;
+    // Try HTML5 geolocation.
+    getLocation();
+    function getLocation() {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+          alert("Geolocation is not supported by this browser.");
+      }
+    }
+
+    function showPosition(position) {
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+      console.log(lat);
+      console.log(lng);
+      plotLocation();
+    }
+
+    var plotLocation = function() {
+      //import php locations array
+      var locations = JSON.parse('<?php echo $myJSON ;?>');
+      //create map
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: new google.maps.LatLng(lat, lng),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+      var infowindow = new google.maps.InfoWindow();
+      //populate map
+      var currMark = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        map: map
+      });
+      var marker, i;
+      for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(locations[i][2], locations[i][1]),
+          map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          return function() {
+            infowindow.setContent(locations[i][0]);
+            infowindow.open(map, marker);
+          }
+        })(marker, i));
+      }
+    }
+  </script>
+  <script async defer
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJCIwyI8OK5-gWujsqqNHk6fVktg5PBzg">
+  </script>
   </body>
 </html>
--- www/test.php
+++ www/test.php
@@ -20,49 +20,58 @@
   </head>
   <body>
     <div id="map"></div>
-    <script>
+    <?php
+      include_once 'connect.php';
 
-      var locations = [
-        ['1802kng', '-80.5260442', '43.4791858'],
-        ['305lester', '-80.5325125', '43.4713316']
-      ];
-      function initMap() {
-        // Try HTML5 geolocation.
+      $mysqli = get_mysqli_conn();
+      
+      $query = "SELECT id, latitude, longitude FROM washrooms";
+      $stmt = $mysqli->prepare($query);
+      $stmt->execute();
+      $stmt->bind_result($wid, $latitude, $longitude);
+      $locations = array();
+      while ($stmt->fetch()) {
+        array_push($locations, (array($wid, $latitude, $longitude)));
+      }
+      $myJSON = json_encode($locations);
+      // var_dump($myJSON);
+    ?>
+
+    <script>
+      var lat,lng;
+      // Try HTML5 geolocation.
+      getLocation();
+      function getLocation() {
         if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position, plotLocation) {
-            var currPos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
+            navigator.geolocation.getCurrentPosition(showPosition);
         } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
+            alert("Geolocation is not supported by this browser.");
         }
       }
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
+      function showPosition(position) {
+        lat = position.coords.latitude;
+        lng = position.coords.longitude;
+        console.log(lat);
+        console.log(lng);
+        plotLocation();
       }
+
       var plotLocation = function() {
-        var map, infoWindow;
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: new google.maps.LatLng(currPos.lat, currPos.lng),
+        //import php locations array
+        var locations = JSON.parse('<?php echo $myJSON ;?>');
+        //create map
+        var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 15,
+          center: new google.maps.LatLng(lat, lng),
           mapTypeId: google.maps.MapTypeId.ROADMAP
         });
-        infoWindow = new google.maps.InfoWindow;
-
+        var infowindow = new google.maps.InfoWindow();
+        //populate map
         var marker, i;
         for (i = 0; i < locations.length; i++) {
           marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            position: new google.maps.LatLng(locations[i][2], locations[i][1]),
             map: map
           });
 
@@ -76,7 +85,7 @@
       }
     </script>
     <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJCIwyI8OK5-gWujsqqNHk6fVktg5PBzg&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJCIwyI8OK5-gWujsqqNHk6fVktg5PBzg">
     </script>
   </body>
 </html>
